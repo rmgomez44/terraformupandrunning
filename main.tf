c092db4c36ea2c07f804d6833b172f0d505c13d2
roberto gomez <rmgomez44@gmail.com>
2021-06-03 18:01:27 -0300

    Added .gitignore + SG for web server added in main.tf

--- main.tf
+++ main.tf
@@ -5,4 +5,22 @@ provider "aws"  {
 resource "aws_instance" "example" {
    ami         = "ami-40d28157"
    instance_type = "t2.micro"
+   vpc_security_group_ids = ["${aws_security_group.web_servers.id}"]
+
+   user_data      = <<-EOF
+                     #!/bin/bash
+                     echo "Hello, World" > index.html
+                     nohup busybox httpd -f -p 8080 &
+                     EOF
    }
+
+resource "aws_security_group" "web_servers" {
+   name = "web_servers"
+
+   ingress {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+   }
+}
